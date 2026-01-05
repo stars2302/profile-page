8a6132dfc83ef3b1df8e6b6f29c3c64638e40dff
Na seo young <seo0@Naui-MacBookAir.local>
2026-01-05 22:52:41 +0900

    feat: more - 기본 리스트 마크업

--- src/pages/more/index.tsx
+++ src/pages/more/index.tsx
@@ -1,10 +1,25 @@
+import { Icon } from "@iconify/react";
+
 function More() {
   return (
-    <div className="contents">
-      <h2>More Info</h2>
-      <p>추가 정보...</p>
+    <div className="more-cont">
+      <ul>
+        {[...Array(20)].map((_, index) => (
+          <li className="more-item">
+            <p className="more-name">
+              lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium ipsa at amet repellat blanditiis similique quis. Rem,
+              iusto consectetur dolorum ipsam officia enim repellat numquam
+              voluptates saepe aperiam voluptatem tempore.
+            </p>
+            <button className="more-btn">
+              <Icon className="icon" icon="mingcute:arrow-right-line" />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default More
+export default More;
