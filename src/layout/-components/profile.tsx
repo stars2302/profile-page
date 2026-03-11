81fbe2794f1365db5ce7486231f10d8f2a45a650
Na seo young <seo0@Naui-MacBookAir.local>
2026-03-11 21:26:38 +0900

    feat: profile - 둥근 부분 svg로 변환

--- src/layout/-components/profile.tsx
+++ src/layout/-components/profile.tsx
@@ -4,12 +4,27 @@ import profileImg from "@/assets/images/person.svg"; // 프로필 이미지
 function Profile() {
   return (
     <>
+      <svg
+        aria-hidden="true"
+        width="0"
+        height="0"
+        style={{ position: "absolute" }}
+      >
+        <defs>
+          <clipPath id="profile-curve-left" clipPathUnits="objectBoundingBox">
+            <path d="M1 0 L1 1 Q0 1 0 0 L1 0 L0 0 L0 1 L1 1 Z" />
+          </clipPath>
+          <clipPath id="profile-curve-right" clipPathUnits="objectBoundingBox">
+            <path d="M0 0 L0 1 Q1 1 1 0 L0 0 L1 0 L1 1 L0 1 Z" />
+          </clipPath>
+        </defs>
+      </svg>
       <div className="profile-area">
         <div className="profile-img-wrap">
           <img src={profileImg} alt="" />
-          <object>
+          {/* <object>
             <embed src={profileImg} />
-          </object>
+          </object> */}
         </div>
         <div className="bg-top-area">
           <div className="left-bg"></div>
