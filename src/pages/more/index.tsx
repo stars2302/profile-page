f86d62a4d3d907ddcd6ca534da14094b9dd9cfc8
Na seo young <seo0@Naui-MacBookAir.local>
2026-07-11 12:32:53 +0900

    docs: more - 프로필페이지 more 내용 추가

--- src/pages/more/index.tsx
+++ src/pages/more/index.tsx
@@ -5,21 +5,27 @@ function More() {
   return (
     <div className="more-cont">
       <ul className="text-list">
-        {moreContents.map(({ id, title, url }) => (
-          <li key={id}>
-            <a
-              className="text-list-item"
-              href={url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <p className="text-list-name text-list-name--ellipsis">{title}</p>
-              <span className="text-list-action" aria-hidden="true">
-                <Icon className="icon" icon="mingcute:arrow-right-line" />
-              </span>
-            </a>
-          </li>
-        ))}
+        {moreContents.map(({ id, projectName, title, type, url }) => {
+          const titlePrefix = type === "profile" ? "프로필 페이지" : projectName ?? "프로젝트";
+
+          return (
+            <li key={id}>
+              <a
+                className="text-list-item"
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="text-list-name text-list-name--ellipsis">
+                  [{titlePrefix}] {title}
+                </p>
+                <span className="text-list-action" aria-hidden="true">
+                  <Icon className="icon" icon="mingcute:arrow-right-line" />
+                </span>
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
